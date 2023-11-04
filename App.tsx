2030a841b434718142b1c9d936b832ca5f1ff916
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-11-04 17:55:56 -0400

    fix(emailInupt): resolving conflicts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import EmailInput from "./src/components/input/index1";
+import EmailInput from "./src/components/EmailInput";
 
 export default function App() {
   return (
