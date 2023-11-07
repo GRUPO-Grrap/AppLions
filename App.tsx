5baea2717b3d6e8365f079756ccdd0fc973dcb7a
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-11-07 00:12:29 -0400

    feat(Toast): create arquive index.tsx

--- App.tsx
+++ App.tsx
@@ -8,25 +8,6 @@ import Input from "./src/components/Input";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Input
-        icon={Mail}
-        placeholder="seuemail@example.com"
-        autoCapitalizeOn="none"
-        autoCorrectOn={false}
-        keyboardType="email-address"
-      />
-      <Input
-        icon={Lock}
-        secureTextEntry
-        placeholder="Sua senha"
-        autoCapitalizeOn="none"
-        autoCorrectOn={false}
-        keyboardType="default"
-      />
-
-      <TouchableOpacity>
-        <Text>Logar</Text>
-      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
