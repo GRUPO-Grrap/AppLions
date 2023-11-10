6a14af8021280ed5c2a5ad8e937ccad7b4b9ee78
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-11-10 19:50:02 -0400

    refactor(ToastNotification): refactor in component

--- App.tsx
+++ App.tsx
@@ -13,7 +13,6 @@ export default function App() {
         toastMode="sucess"
         legenda="Verifique se os campos foram preenchidos corretamente"
       />
-
       <StatusBar style="auto" />
     </View>
   );
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,4 +1,5 @@
-import { AlertCircle } from "lucide-react-native";
+import { AlertCircle, CheckCircle2 } from "lucide-react-native";
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 interface ToastMode {
@@ -6,18 +7,24 @@ interface ToastMode {
   toastMode: string;
 }
 
-const sucess =
-  "bg-green-100 w-11/12 border-l-8 border-green-400 px-4 py-2 flex-row items-center h-20 rounded-lg";
-const error =
-  "bg-red-100 w-11/12 border-l-8 border-red-400 px-4 py-2 flex-row items-center h-20 rounded-lg";
-
 const ToastNotification = ({ toastMode, legenda }: ToastMode) => {
+  const sucess =
+    "bg-green-100 w-11/12 border-l-8 border-green-400 px-4 py-2 flex-row items-center h-20 rounded-lg";
+
+  const error =
+    "bg-red-100 w-11/12 border-l-8 border-red-400 px-4 py-2 flex-row items-center h-20 rounded-lg";
+
   const selectedStyle = toastMode === "sucess" ? sucess : error;
+  const [icon, setIcon] = useState(selectedStyle);
 
   return (
     <View className={selectedStyle}>
-      <AlertCircle color="rgb(248, 113, 113)" size={28} />
-      <View className="px-4">
+      {toastMode ? (
+        <CheckCircle2 color="rgb(74, 222, 128)" size={28} />
+      ) : (
+        <AlertCircle color="rgb(248, 113, 113)" size={28} />
+      )}
+      <View className="px-4 ">
         <Text className="font-normal text-base p-1">{legenda}</Text>
       </View>
     </View>
